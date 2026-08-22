0a962e1c41596c141e06d82f68e646ca63150917
katjamva <katja.myrskyvaara@gmail.com>
2026-08-22 20:04:21 +0300

    Add production frontend to phonebook backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('body', (request) => {
     return JSON.stringify(request.body)
 })
@@ -93,8 +96,7 @@ app.post('/api/persons', (request, response) => {
 
 })
 
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
