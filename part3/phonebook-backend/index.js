9402800658225b447e2951b1d35eb95d3509d810
Myrskyvaara Katja <myrskyva@cubbli24-gold.cs.helsinki.fi>
2026-09-11 10:57:51 +0300

    deleting entries reflected in db

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -50,20 +50,33 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            response.status(400).send({error:'malformatted id'})
+        })
+   // const id = request.params.id
+   // const person = persons.find(person => person.id === id)
+    //if (person) {
+      //  response.json(person)
+   // } else {
+   //     response.status(404).end()
+    //}
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(person => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const generateId = () => {
