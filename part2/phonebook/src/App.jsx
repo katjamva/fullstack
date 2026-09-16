f42529eced6e684ea84b8d2c2ca1ac5296966525
Myrskyvaara Katja <myrskyva@cubbli24-gold.cs.helsinki.fi>
2026-09-16 10:49:45 +0300

    add error message for validation error

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -155,7 +155,9 @@ const App = () => {
         setTimeout(() => setAddedMessage(null), 5000)
       })
       .catch((error) => {
-        console.error(error)
+        console.error(error.response.data.error)
+        setErrorMessage(error.response.data.error)  
+        setTimeout(() => setErrorMessage(null), 5000)
       })
   }
 
